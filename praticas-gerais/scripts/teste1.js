937671d5d7b9cf551b6cfaf07e4640a0f26212fd
Eldersm <122117900+Eldersm@users.noreply.github.com>
2023-02-10 08:00:39 -0300

    update

---
+++ praticas-gerais/scripts/teste1.js
@@ -0,0 +1,12 @@
+/*function sum(number1 , number2){
+    const soma = number1 + number2
+    return soma
+
+}*/
+
+
+const sum = (number1, number2) => {
+    return number1 + number2
+
+}
+console.log(sum(2,3))
