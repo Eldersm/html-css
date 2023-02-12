9d54c7f9cd5b3213153aa90f94c52cbb19b82748
Eldersm <122117900+Eldersm@users.noreply.github.com>
2023-02-12 13:20:36 -0300

    versão 1.0

--- praticas-gerais/scripts/teste1.js
+++ praticas-gerais/scripts/teste1.js
@@ -1,5 +1,7 @@
 let res =document.querySelector('input#inumero')
 let numeros = []
+let header =document.querySelector('header#cabe')
+header.createElement('img')
 
 
 function somar(){
@@ -10,23 +12,59 @@ function somar(){
 function dividir(){
     numeros[0]=parseFloat(res.value)
     res.value = ' '
-    res.value = ' /'
+    res.value = '/'
 }
+function subtrair(){
+    numeros[0]=parseFloat(res.value)
+    res.value = ' '
+    res.value = '-'
+}
+function multiplicar(){
+    numeros[0]= parseFloat(res.value)
+    res.value = ' '
+    res.value = '*'
+}
+function limpa(){
+    let limpa = res.value
+    res.value = limpa.substr(0,limpa.length -1)
+    res.focus()
+
+}
+
 
 function calculo(){
    let total = 0
    numeros[1]= Number(res.value)
-
+   let limpa = res.value
     if(res.value.includes('+')){
-        total = numeros[0] / numeros[1]
+        res.value = limpa.substr(1)
+        total = numeros[0] + numeros[1]
         res.value = total
+        res.focus()
         return res.value
     }
     else if (res.value.includes('/')){
-        
+        res.value = limpa.substr(1)
         numeros[1]= Number(res.value)
-        total = numeros[0] + numeros[1]
+        total = numeros[0] / numeros[1]
         res.value = total
+        res.focus()
         return res.value
+    }else if(res.value.includes('-')){
+        res.value = limpa.substr(1)
+        numeros[1]=Number(res.value)
+        total=numeros[0] - numeros[1]
+        res.value = total
+        res.focus()
+        return res.value
+    }else if (res.value.includes('*')){
+        res.value = limpa.substr(1)
+        numeros[1]=Number(res.value)
+        total = numeros[0] * numeros[1]
+        res.value = total
+        res.focus()
+        return res.value
+
+
     }
 }
